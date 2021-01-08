04c4655ef9b11341b9b20eb186a27c8a6c467bb5
Dzun <nurroinz@gmail.com>
2021-01-08 20:41:54 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,15 +27,15 @@ html {
 .button--green {
   display: inline-block;
   border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+  border: 1px solid #52A2F3;
+  color: #52A2F3;
   text-decoration: none;
   padding: 10px 30px;
 }
 
 .button--green:hover {
   color: #fff;
-  background-color: #3b8070;
+  background-color: #52A2F3;
 }
 
 .button--grey {
